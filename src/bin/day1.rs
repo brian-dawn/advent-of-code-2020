73a8587843b8953ccec8d5649deb3daf4600efd0
Brian Dawn <brian.t.dawn@gmail.com>
2020-12-01 08:14:07 -0600

    Initialize the repo and solve day1.

---
+++ src/bin/day1.rs
@@ -0,0 +1,58 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use anyhow::{Context, Result};
+
+fn read_input() -> Result<Vec<i32>> {
+    let input = File::open("input/day1.txt")?;
+    let buffered = BufReader::new(input);
+
+    buffered
+        .lines()
+        .map(|line| Ok(line?.parse::<i32>()?))
+        .collect()
+}
+
+fn part1(input: &[i32]) -> Option<i32> {
+    for (ix, x) in input.iter().enumerate() {
+        for (iy, y) in input.iter().enumerate() {
+            if iy == ix {
+                continue;
+            }
+            if x + y == 2020 {
+                return Some(x * y);
+            }
+        }
+    }
+
+    None
+}
+
+fn part2(input: &[i32]) -> Option<i32> {
+    for (ix, x) in input.iter().enumerate() {
+        for (iy, y) in input.iter().enumerate() {
+            for (iz, z) in input.iter().enumerate() {
+                if iy == ix || iz == ix || iz == iy {
+                    continue;
+                }
+                if x + y + z == 2020 {
+                    return Some(x * y * z);
+                }
+            }
+        }
+    }
+
+    None
+}
+
+fn main() -> Result<()> {
+    let input = read_input()?;
+
+    let answer1 = part1(&input).context("Failed to find an answer for part1")?;
+    println!("part1: {}", answer1);
+
+    let answer2 = part2(&input).context("Failed to find an answer for part2")?;
+    println!("part2: {}", answer2);
+
+    Ok(())
+}

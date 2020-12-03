6647fa38431329cf76ee3a3c1a40c4dc72656cf1
Brian Dawn <brian.t.dawn@gmail.com>
2020-12-03 16:05:39 -0600

    Cave to peer pressure.

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -61,10 +61,10 @@ fn main() -> Result<()> {
     println!("part1: {}", num_trees(&world, 3, 1));
 
     let rules = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-    let part2 = rules
+    let part2: usize = rules
         .iter()
         .map(|(dx, dy)| num_trees(&world, *dx, *dy))
-        .fold(1, |acc, val| acc * val);
+        .product();
 
     println!("part2: {}", part2);
 

d4c16abbe567922967e5e61a0138cb82ace14d2d
Brian Dawn <brian.t.dawn@gmail.com>
2020-12-05 13:39:17 -0600

    Use tuple windows instead of windows.

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -12,14 +12,14 @@ fn read_input() -> Result<Vec<String>> {
     buffered.lines().map(|l| Ok(l?)).collect()
 }
 
-fn seat_id(pass: &str) -> Option<u16> {
+fn seat_id(pass: &str) -> Option<i64> {
     let bin_string = pass
         .replace("F", "0")
         .replace("B", "1")
         .replace("L", "0")
         .replace("R", "1");
 
-    u16::from_str_radix(&bin_string, 2).ok()
+    i64::from_str_radix(&bin_string, 2).ok()
 }
 
 fn main() -> Result<()> {
@@ -37,10 +37,10 @@ fn main() -> Result<()> {
         .iter()
         .filter_map(|pass| seat_id(pass))
         .sorted()
-        .collect_vec()
-        .windows(2)
-        .find(|window| window[0] + 1 != window[1])
-        .context("failed to find our seat!")?[0]
+        .tuple_windows::<(_, _)>()
+        .find(|(a, b)| a + 1 != *b)
+        .context("failed to find our seat!")?
+        .0
         + 1;
 
     println!("part2: {}", part2);
